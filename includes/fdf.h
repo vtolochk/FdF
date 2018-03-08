ca52dd7a62f3244ac273be6849a963be4dd2d4d2
vtolochk <v.tolochko13@gmail.com>
2018-03-08 17:24:57 +0200

    the first commit in this project

---
+++ includes/fdf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtolochk <vtolochk@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/08 16:00:00 by vtolochk          #+#    #+#             */
+/*   Updated: 2018/03/08 16:00:00 by vtolochk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include <mlx.h>
+#include <math.h>
+#include "../Libft/includes/libft.h"
+
+# define ERROR 1
+# define SUCCESS 0
+
+#endif
